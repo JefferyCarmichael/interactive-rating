924944487185d01ce46e9fbd64ef254c5df5ec01
Jeff <jeff.c.carmichael@gmail.com>
2022-05-16 13:55:40 -0400

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import RatingCard from "./components/RatingCard";
+import ThankYou from "./components/ThankYou";
+import Footer from "./components/Footer";
+import "./App.css";
 
 function App() {
+  const [score, setScore] = useState(0);
+  const [survey, setSurvey] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="app">
+        <div className="rating-container">
+          {survey === 0 ? (
+            <RatingCard
+              score={score}
+              setScore={setScore}
+              survey={survey}
+              setSurvey={setSurvey}
+            />
+          ) : (
+            <ThankYou score={score} setSurvey={setSurvey} setScore={setScore} />
+          )}
+        </div>
+      </div>
+      <Footer />
+    </>
   );
 }
 
