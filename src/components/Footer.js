d1534ec6a865ba5f8dd30753bb04697108a41acd
Jeff <jeff.c.carmichael@gmail.com>
2022-05-16 14:05:27 -0400

    added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="attribution">
+    Challenge by&nbsp; <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>.&nbsp; 
+    Coded by &nbsp;<a href="#">Your Name Here</a>.
+  </div>
+  )
+}
+
+export default Footer
